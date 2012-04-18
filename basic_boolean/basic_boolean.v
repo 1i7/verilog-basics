7ce936fc3ebbd6909962ad903def92fd93236102
1i7 <1i7@v1-0.ru>
2012-04-19 00:58:47 +0400

    Add comments for boolean operators

--- basic_boolean/basic_boolean.v
+++ basic_boolean/basic_boolean.v
@@ -3,10 +3,10 @@ module basic_boolean(input a, input b,
     output a_or_b, output a_nand_b
     );
 
-assign not_a = ~a;
-assign not_b = ~b;
-assign a_and_b = a & b;
-assign a_or_b = a | b;
-assign a_nand_b = ~(a & b);
+assign not_a = ~a; // NOT
+assign not_b = ~b; // NOT
+assign a_and_b = a & b; // AND
+assign a_or_b = a | b; // OR
+assign a_nand_b = ~(a & b); // NAND
 
 endmodule
