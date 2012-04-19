20ef75f9977d9c4580bc322f19605714f046da0f
1i7 <1i7@v1-0.ru>
2012-04-19 09:17:43 +0400

    Fix seven_driver for common anode mode

--- seven_driver/seven_driver.v
+++ seven_driver/seven_driver.v
@@ -6,13 +6,16 @@ module seven_driver(
 	 // value=true - 7
 	 // value=false - 5
 	 
-	 assign segments[0] = value ? 0 : 0;
-	 assign segments[1] = value ? 0 : 1;
-	 assign segments[2] = value ? 1 : 1;
-	 assign segments[3] = value ? 1 : 1;
-	 assign segments[4] = value ? 1 : 0;
-	 assign segments[5] = value ? 1 : 1;
-	 assign segments[6] = value ? 0 : 1;
-	 assign segments[7] = value ? 0 : 1;
+	 // common anode display: 1 = OFF, 0 = ON
+	 
+	 assign segments[0] = value ? 1 : 1;
+	 assign segments[1] = value ? 1 : 0;
+	 assign segments[2] = value ? 0 : 0;
+	 assign segments[3] = value ? 0 : 0;
+	 assign segments[4] = value ? 0 : 1;
+	 assign segments[5] = value ? 0 : 0;
+	 assign segments[6] = value ? 1 : 0;
+	 assign segments[7] = value ? 1 : 0;
 
 endmodule
+
